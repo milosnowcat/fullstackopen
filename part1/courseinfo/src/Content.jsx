90e97f95cf200021b5abf3899963eb643165d5aa
milosnowcat <ramiro.alvarez@rahcode.com>
2024-02-19 16:36:43 -0600

    1.4: información del curso paso 4

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -3,9 +3,9 @@ import Part from "./Part";
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1.name} exercises={props.part1.exercises}></Part>
-      <Part part={props.part2.name} exercises={props.part2.exercises}></Part>
-      <Part part={props.part3.name} exercises={props.part3.exercises}></Part>
+      {props.parts.map((part, index) => (
+        <Part key={index} part={part.name} exercises={part.exercises} />
+      ))}
     </div>
   );
 };
--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -1,7 +1,17 @@
+const countExersices = (parts) => {
+  let totalExercises = 0;
+
+  parts.forEach((part) => {
+    totalExercises += part.exercises;
+  });
+
+  return totalExercises;
+};
+
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.exercises}</p>
+      <p>Number of exercises {countExersices(props.parts)}</p>
     </div>
   );
 };
